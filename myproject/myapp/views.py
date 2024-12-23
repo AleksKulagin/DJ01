03e0cd2d7c6c527fc55c3a3ebf81f3e3838ce7e1
AleksKulagin <master-tech@yandex.ru>
2024-12-23 16:50:23 +1000

    1

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,14 +3,30 @@ from django.shortcuts import render
 
 def home(request):
     # Представление для главной страницы
-    return render(request, 'home.html')
+    data = {
+        'caption': "Переменная из <views.py>"
+    }
+    return render(request, 'myapp/home.html', data)
 
 
 def data(request):
     # Представление для страницы data
-    return render(request, 'data.html')
+    data = {
+        'caption': "Переменная из <views.py>"
+    }
+    return render(request, 'myapp/data.html', data)
 
 
 def test(request):
     # Представление для страницы test
-    return render(request, 'test.html')
+    data = {
+        'caption': "Переменная из <views.py>"
+    }
+    return render(request, 'myapp/test.html', data)
+
+def test2(request):
+    # Представление для страницы test2
+    data = {
+        'caption': "Переменная из <views.py>"
+    }
+    return render(request, 'myapp/test2.html', data)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,15 +1,22 @@
+from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
+
+    path('news/', include('news.urls')),
+
     path('', views.home, name='home'),  # Главная страница
     path('data/', views.data, name='data'),  # Страница data
     path('test/', views.test, name='test'),  # Страница test
-
-]
+    path('test2/', views.test2, name='test2'),  # Страница test2
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
